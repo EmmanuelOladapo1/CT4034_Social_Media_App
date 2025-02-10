014d2fdcb48639947af3d5d5eda9f1f704e74afe
Emmanuel Oladapo <emmanueloladapo365@gmail.com>
2025-02-10 11:11:37 +0000

    Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
      modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,11 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 's4413678_db');
-define('DB_USER', 's4413678_social');
-define('DB_PASS', '7q0fe22B~');
+$config = include_once 'config.php';
 
-try {
-    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
+$host = $config{
+'DB_HOST'};
+$db = $config{
+'DB_NAME'};
+$user = $config{
+'DB_USER'};
+$pass = $config{
+'DB_PASS'};
