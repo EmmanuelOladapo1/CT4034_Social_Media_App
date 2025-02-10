e2b52a17453c2fbcc23874671e856690880af49d
Emmanuel Oladapo <emmanueloladapo365@gmail.com>
2025-02-10 10:54:24 +0000

    Changes to be committed:

--- config/database.php
+++ config/database.php
@@ -5,9 +5,8 @@ define('DB_USER', 's4413678_social');
 define('DB_PASS', '7q0fe22B~');
 
 try {
-    $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
-?>
