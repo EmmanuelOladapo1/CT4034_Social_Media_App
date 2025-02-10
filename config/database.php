934572b1d8a3d05f339eaaccbd4e76f75a31886a
Emmanuel Oladapo <emmanueloladapo365@gmail.com>
2025-02-10 11:14:10 +0000

    Chnages to be committed:   modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,7 @@
 <?php
 $config = include_once 'config.php';
 
-$host = $config{
-'DB_HOST'};
-$db = $config{
-'DB_NAME'};
-$user = $config{
-'DB_USER'};
-$pass = $config{
-'DB_PASS'};
+$host = $config['DB_HOST'];
+$db = $config['DB_NAME'];
+$user = $config['DB_USER'];
+$pass = $config['DB_PASS'];
