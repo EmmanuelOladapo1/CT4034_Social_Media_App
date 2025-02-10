ccd051496568af3de9c90703b9c69f7c2637b64d
Emmanuel Oladapo <emmanueloladapo365@gmail.com>
2025-02-10 12:27:39 +0000

    Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
      modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -5,3 +5,14 @@ $host = $config['DB_HOST'];
 $db = $config['DB_NAME'];
 $user = $config['DB_USER'];
 $pass = $config['DB_PASS'];
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // echo "Connected successfully";
+} catch (PDOException $e) {
+  die("Connection failed: " . $e->getMessage());
+}
+
+// Make connection available globally
+global $conn;
